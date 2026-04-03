3415b1a584e90c45d5974ef3a469ce8feb944060
Bombella <nmasionyena@gmail.com>
2026-04-03 23:43:57 +0100

    First easter eggs

---
+++ meatPie/Addition.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+	public class Addition{
+	public static void main(String[] args){
+	Scanner input= new Scanner(System.in);
+	System.out.print("Enter the first number = ");
+	int number1=input.nextInt();
+	System.out.print("Enter the second number = ");
+	int number2=input.nextInt();
+		System.out.printf("sum of %d+%d is = %d ", number1, number2, (number1+number2));
+	
+ 
+	
+}
+
+
+
+
+}
+
+
---
+++ meatPie/Age.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+	public class Age{
+		public static void main(String...args){
+			Scanner inputAge = new Scanner(System.in);
+				System.out.print("What is your age?");
+	
+
+
+}
+
+
+
+}
+
+
+
