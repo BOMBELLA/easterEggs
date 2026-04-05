4e346230015a181c9772e92d932396e1eb167af3
Bombella <nmasionyena@gmail.com>
2026-04-05 12:05:57 +0100

    first cube

---
+++ meatPie/Cube.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+	 public class Cube{
+		public static void main(String[]args){
+		Scanner inputCube = new Scanner(System.in);
+		System.out.print("Enter the number");
+		int number = inputCube.nextInt();
+		int cube = number*number*number;
+		System.out.printf("Cube is %d", cube);
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
