f4ef44386c5cd294122f5fe4806f0ed07d3297fc
Bombella <nmasionyena@gmail.com>
2026-04-05 06:26:53 +0100

    first product

---
+++ meatPie/Multiplication.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+	public class Multiplication{
+	public static void main(String[]args){
+	Scanner inputProduct = new Scanner(System.in);
+	System.out.print("Enter first number =  ");
+	int number1 = inputProduct.nextInt();
+	System.out.print("Enter second number = ");
+	int number2 = inputProduct.nextInt();
+	int product = number1 * number2;
+	System.out.printf("Product is  %d",product);
+	
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+}
+			
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
