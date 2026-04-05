774cbfd13131b701ec8858ab708a86d8b7facabb
Bombella <nmasionyena@gmail.com>
2026-04-05 14:08:45 +0100

    Favourite colour

---
+++ meatPie/Colour.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+	public class Colour{
+	public static void main(String[]args){
+	System.out.println("What is your favourite colour?");
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
