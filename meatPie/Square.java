b2bd15ceca74fa7c709b258ba4bc2664b637dae4
Bombella <nmasionyena@gmail.com>
2026-04-05 12:08:51 +0100

    first square

---
+++ meatPie/Square.java
@@ -0,0 +1,34 @@
+import java.util.Scanner; 	
+public class Square{
+	public static void main(String...args){
+	Scanner inputSquare = new Scanner(System.in);
+	System.out.print("Enter the number = ");
+	int number = inputSquare.nextInt();
+	int square   = number * number;
+	System.out.printf("Square is %d" , square);
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}                                                                                                                                                                                                                                                  
