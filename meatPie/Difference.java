400f0db9ea28b60fd6ff328991bbfa64611e2ff4
Bombella <nmasionyena@gmail.com>
2026-04-05 01:06:30 +0100

    first Difference

---
+++ meatPie/Difference.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+	public class Difference{
+	public static void main(String[]args){
+	Scanner inputMinus = new Scanner(System.in);
+		System.out.print("Enter the frist number = ");
+		int number1 = inputMinus.nextInt();
+		System.out.print("Enter the second number = ");
+		int number2 = inputMinus.nextInt();
+		int difference = number1 - number2;
+       System.out.printf("Difference is %d - %d is = %d ",difference);
+	
+
+
+
+
+
+
+
+
+
+
+
+}
+		
+
+
+
+
+}
+			
